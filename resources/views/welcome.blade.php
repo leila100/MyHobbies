9444b0505aeac26c81a0a25b02b292d1a00aa97f
Leila <nb.leila10@gmail.com>
2020-10-06 17:59:32 -0500

    change welcome page to display the user's name if logged in

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>My Hobbies</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Hello
+                Hello {{auth()->user()->name ?? ''}}
             </div>
 
             <div class="links">
