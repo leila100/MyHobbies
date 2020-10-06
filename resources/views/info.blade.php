a4e0d5017febfecb62601949c2241333f5f926f1
Leila <nb.leila10@gmail.com>
2020-10-06 18:24:08 -0500

    Add two new pages: start and info

---
+++ resources/views/info.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Info</div>
+
+                <div class="card-body">
+                    But do you know how many kids HE has?
+
+                    I was in a meeting recently where a big, new opportunity was being discussed and a very capable woman's name was put in the ring for consideration. And someone said "she is pretty busy, a big job and her 2 kids are young". After that a MAN in the room said, "we also just talked about this same opportunity for several men - and not once did we mention how many children they had."
+
+                    I am just returning from maternity leave after having our 4th little girl. And this was a great reminder, that it's going to take both men and women in leadership to give women - not just equal pay, but equal opportunities. Regardless of how many kids, pets or plants we have depending on us.
+
+                    For those young women wondering 'can I do both?' - you can. And it's wonderful.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/starting_page.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Starting Page</div>
+
+                <div class="card-body">
+                    When we seek to discover the best in others, we somehow bring out the best in ourselves.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
