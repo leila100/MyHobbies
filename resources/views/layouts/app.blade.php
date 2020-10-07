06d91419ad7387b41e5aa89151ee7b3a1bd43130
Leila <nb.leila10@gmail.com>
2020-10-07 12:52:02 -0500

    Add form validation on individual form fields

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@
             @if($errors->any())
             <div class="container">
                 <div class="alert alert-danger" role="alert">
-                    <ul>
+                    <ul class="mb-0">
                         @foreach($errors->all() as $error)
                         <li>{!! $error !!}</li>
                         @endforeach
