169455ee5067f425d33d687d37778a97566b0574
Leila <nb.leila10@gmail.com>
2020-10-08 11:27:25 -0500

    Add model tag and it's corrsponding routes and index view

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::get('/info', function () {
 
 Route::resource('hobby', 'HobbyController');
 
+Route::resource('tag', 'TagController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
