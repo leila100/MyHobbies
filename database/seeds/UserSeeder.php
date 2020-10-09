20c4a593d3b95b39e40cf5b7641eee80eff8e7fa
Leila <nb.leila10@gmail.com>
2020-10-09 13:15:23 -0500

    Add seeding of hobbies for each user during users seeding

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,6 +11,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 100)->create();
+        factory(App\User::class, 100)->create()->each(function ($user) {
+            factory(App\Hobby::class, rand(1, 8))->create([
+                'user_id' => $user->id
+            ]);
+        });
     }
 }
