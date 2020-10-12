9300775fc0af0e144ab36e3774c3240561d0c8c5
Leila <nb.leila10@gmail.com>
2020-10-12 18:34:33 -0500

    Display user hobbies in user details page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -47,7 +47,7 @@ class UserController extends Controller
     public function show(User $user)
     {
         return view('user.show')->with([
-            'user' => $user
+            'user' => $user,
         ]);
     }
 
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -6,20 +6,34 @@
     <div class="row justify-content-center">
         <div class="col-md-11">
             <div class="card">
-                <div class="card-header">{{ $user->name }}</div>
+                <div class="card-header" style="font-size: 150%">{{ $user->name }}</div>
                 <div class="card-body">
-                    <b>My motto:</b><br>{{ $user->motto }}
+                    <h5><b>My motto:</b></h5>{{ $user->motto }}
                     <br>
-                    <p class="mt-2"><b>About Me:</b><br>{{ $user->about_me }}</p>
-                    <h4>Hobbies</h4>
-                    <ul>
-                    @foreach ($user->hobbies as $hobby)
-                        <li>{{$hobby->name}}</li>
-                    @endforeach
-                    </ul>
+                    <p class="mt-2">
+                        <h5><b>About Me:</b></h5>
+                        {{ $user->about_me }}
+                    </p>
+                    @if ($user->hobbies->count() == 0)
+                        <h5><b>{{ $user->name }} has no hobbies.</b></h5>
+                    @else
+                        <h5><b>{{ $user->name }}'s hobbies</b></h5>
+                        <ul class="list-group">
+                            @foreach($user->hobbies as $hobby)
+                                <li class="list-group-item">
+                                    <a title="Show Details" href="/hobby/{{ $hobby->id }}">{{ $hobby -> name }}</a>
+                                    <span class="float-right mx-2">{{ $hobby->created_at->diffForHumans() }}</span>
+                                    <br>
+                                    @foreach ($hobby->tags as $tag)
+                                    <a href="/hobby/tag/{{$tag->id}}"><span class="badge badge-{{$tag->style}}">{{$tag->name}}</span></a>
+                                    @endforeach
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
                 </div>
             </div>
-            <div class="mt-2">
+            <div class="mt-4">
                 <a href="{{URL::previous()}}" class="btn btn-primary btn-sm"><i class="fas fa-arrow-circle-left"></i> Back to Hobbies</a>
             </div>
         </div>
