2fca35bdfb2ee8609666eaa50dcf34b7345d6273
Leila <nb.leila10@gmail.com>
2020-10-12 18:12:53 -0500

    Show user hobbies in the home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Hobby;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $hobbies = Hobby::select()->where('user_id', auth()->id())->orderby('updated_at', 'DESC')->get();
+        return view('home')->with([
+            'hobbies' => $hobbies
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-11">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
                 <div class="card-body">
@@ -13,7 +13,37 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    <br>
+                    @isset($hobbies)
+                        @if ($hobbies->count() == 0)
+                            No hobbies.
+                        @else
+                            <h3>Your hobbies</h3>
+                            <ul class="list-group">
+                                @foreach($hobbies as $hobby)
+                                <li class="list-group-item">
+                                    <a title="Show Details" href="/hobby/{{ $hobby->id }}">{{ $hobby -> name }}</a>
+                                    @auth
+                                    <a class="btn btn-light btn-sm ml-2" href="/hobby/{{ $hobby->id }}/edit" title="Edit hobby"><i class="fas fa-pen"></i> Edit</a>
+                                    @endauth
+                                    @auth
+                                    <form class="float-right" action="/hobby/{{ $hobby->id }}" method="POST" style="display:inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input type="submit" class="btn btn-outline-danger btn-sm" value="Delete">
+                                    </form>
+                                    @endauth
+                                    <span class="float-right mx-2">{{ $hobby->created_at->diffForHumans() }}</span>
+                                    <br>
+                                    @foreach ($hobby->tags as $tag)
+                                    <a href="/hobby/tag/{{$tag->id}}"><span class="badge badge-{{$tag->style}}">{{$tag->name}}</span></a>
+                                    @endforeach
+                                </li>
+                                @endforeach
+                            </ul>
+                        @endif
+                    @endisset
+                    <br>
                     <a href="/hobby/create" class="btn btn-success btn-sm"><i class="fas fa-plus-circle"></i> Create New Hobby</a>
                 </div>
             </div>
