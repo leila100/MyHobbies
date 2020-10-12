8fe029abe06b4d7623ce9af706020f6a91fc74e2
Leila <nb.leila10@gmail.com>
2020-10-12 12:52:10 -0500

    Add page not found (404) page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page not found - 404</title>
+</head>
+<body style="background: url('/404Background.jpg'); background-size: cover; background-repeat: no-repeat; background-position: center">
+    <div style="margin-bottom: 200px; text-align: center; position: relative; top: 70px;">
+        <h1 style="margin-top: 500px;">OHHHHHHH MYYYYY GOOOOOOOOOOOOOOD!!!!! <br><strong>404</strong></h1>
+        <h2>The page could not be found.</h2>
+    </div>
+</body>
+</html>
