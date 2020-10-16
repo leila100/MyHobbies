d48ca788587225f1878b9afb0b9369e53885a43f
Leila <nb.leila10@gmail.com>
2020-10-15 19:00:08 -0500

    Add hobby policy
    Do not allow a user to edit/delete another user's hobbies

--- app/Http/Controllers/HobbyController.php
+++ app/Http/Controllers/HobbyController.php
@@ -7,6 +7,7 @@ use App\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Gate;
 //use Illuminate\Support\Carbon; // Used to format time/dates
 
 class HobbyController extends Controller
@@ -108,6 +109,8 @@ class HobbyController extends Controller
      */
     public function edit(Hobby $hobby)
     {
+        abort_unless(Gate::allows('update', $hobby), 403);
+
         return view('hobby.edit')->with([
             'hobby' => $hobby,
             'message_success' => Session::get('message_success'),
@@ -124,6 +127,8 @@ class HobbyController extends Controller
      */
     public function update(Request $request, Hobby $hobby)
     {
+        abort_unless(Gate::allows('update', $hobby), 403);
+
         $request->validate([
             'name' => 'required|min:3',
             'description' => 'required|min:5',
@@ -151,6 +156,7 @@ class HobbyController extends Controller
      */
     public function destroy(Hobby $hobby)
     {
+        abort_unless(Gate::allows('delete', $hobby), 403);
         $oldName = $hobby->name;
         $hobby->delete();
         return $this->index()->with([
